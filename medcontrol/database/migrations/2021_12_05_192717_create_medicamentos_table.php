181d47385bb249d7583cd74fe49ff64dc18183d7
ivannenicole <ivanne.n@escolar.ifrn.edu.br>
2021-12-10 14:12:16 -0300

    banco de dados criado e tela inicial editada

---
+++ medcontrol/database/migrations/2021_12_05_192717_create_medicamentos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedicamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicamentos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->integer('dosagem');
+            $table->text('prescricao');
+            $table->integer('quantidade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicamentos');
+    }
+}
